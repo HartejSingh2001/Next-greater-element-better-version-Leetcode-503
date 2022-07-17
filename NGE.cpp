e4402fae8a18d49d602d9440e834a69b3ecb8ac0
Hartej Singh <86732914+HartejSingh2001@users.noreply.github.com>
2022-07-17 22:07:08 +0530

    Create NGE.cpp

---
+++ NGE.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size(),i;
+        stack<int> st;
+        vector<int> v(n,-1);// make a vector of size n and assign -1 to all of them
+        for(i=(2*n)-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=nums[i%n])
+            {
+                st.pop();
+            }
+            if(i<n)
+            {
+                if(!st.empty())
+                {
+                    v[i]=st.top();
+                    //v.push_back(st.top());wrong answer
+                }
+            }
+            st.push(nums[i%n]);
+        }
+     return v;   
+    }
+};
